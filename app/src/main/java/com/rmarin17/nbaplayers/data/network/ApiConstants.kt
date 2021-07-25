220abc963435466d7737f1ff9f2757484047551e
ricardo-marin-globant <ricardo.marin@globant.com>
2021-07-25 15:12:18 -0500

    Add RX Java, Retrofit and endpoint calls. - Update dependencies.gradle and build.gradle to add RXJava and Retrofit dependencies. - Add PlayerResponseModel.kt and TeamResponseModel.kt to handle the response. - Add ApiConstants.kt to extract all the constants related Api into one file. - Add PlayerServices.kt to defines the endpoints of the app. - Update AppModule.kt to inject Retrofit builder and

---
+++ app/src/main/java/com/rmarin17/nbaplayers/data/network/ApiConstants.kt
@@ -0,0 +1,10 @@
+package com.rmarin17.nbaplayers.data.network
+
+/**
+ * Constants of the api service.
+ */
+object ApiConstants {
+    const val URL_BASE = "https://balldontlie.io/api/v1/"
+    const val SEARCH = "search"
+    const val SEARCH_PATCH ="/players"
+}
